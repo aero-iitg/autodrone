13730cb0aa6dc5734d12bca1ac87e17effc411d1
pravadhanreddy <pravadhan.green1998@gmail.com>
2019-02-06 01:43:49 +0530

    add ros workspace

---
+++ sim_ws/build/catkin_generated/order_packages.py
@@ -0,0 +1,5 @@
+# generated from catkin/cmake/template/order_packages.context.py.in
+source_root_dir = "/home/aero/sim_ws/src"
+whitelisted_packages = "".split(';') if "" != "" else []
+blacklisted_packages = "".split(';') if "" != "" else []
+underlay_workspaces = "/home/aero/sim_ws/devel;/opt/ros/melodic".split(';') if "/home/aero/sim_ws/devel;/opt/ros/melodic" != "" else []
